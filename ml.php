24feb54a0bdd1fce26409c5b6484a70fe30ef2b5
Amandine-2021 <90973118+Amandine-2021@users.noreply.github.com>
2021-09-22 16:29:12 -0600

    Add files via upload

---
+++ ml.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstName"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<br>Your comments were <?php echo $_POST["content"]; ?>
+
+</body>
+</html>
